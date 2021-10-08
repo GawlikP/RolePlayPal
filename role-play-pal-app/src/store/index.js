df91689f7f7dcb2afc5862481cff6270ced144c1
GawlikP <rudepeeter@gmail.com>
2021-10-08 22:33:37 +0200

    Created working login form. With primitive error handling.

--- role-play-pal-app/src/store/index.js
+++ role-play-pal-app/src/store/index.js
@@ -2,11 +2,20 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user: { 
+      token: "",
+      username: ""
+    }
   },
   mutations: {
+    logUser(state, dat) {
+      state.user.token = dat.token
+      state.user.username = dat.username
+    }
   },
   actions: {
   },
   modules: {
+   
   }
 })
